4fb2b4fb982efb1ee10b605d8467c020845a5fb3
Jordan G <jorg2332@gmail.com>
2018-06-29 21:36:33 -0600

    added functions and variables to ShoppingCart header

--- Lab_8/ShoppingCart.h
+++ Lab_8/ShoppingCart.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
+
+#include "ItemToPurchase.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+class ShoppingCart
+{
+	public:
+		string GetName();
+		string GetDate();
+		void AddItemToCart(ItemToPurchase);
+		void RemoveItemFromCart(string itemName);
+		void UpdateItemQuantity(string, int);
+		int GetNumOfItems();
+		double GetTotalCost();
+		void PrintItemTotalsAndCosts();
+		void PrintItemDescriptions();
+	private:
+		string customerName;
+		string dateCreated;
+		vector<ItemToPurchase> items;
+};
+#endif

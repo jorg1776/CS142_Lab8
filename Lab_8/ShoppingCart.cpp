035b155bffee91636f63b67d3a6f66738321da86
Jordan G <jorg2332@gmail.com>
2018-06-29 21:56:38 -0600

    added functionalities to functions

--- Lab_8/ShoppingCart.cpp
+++ Lab_8/ShoppingCart.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
-
 #include "ShoppingCart.h"
+#include <iostream>
+using namespace std;
 
 string ShoppingCart::GetCustomerName() { return customerName; }
 
@@ -8,14 +9,70 @@ string ShoppingCart::GetDateCreated() { return dateCreated; }
 
 void ShoppingCart::AddItemToCart(ItemToPurchase item) {	items.push_back(item); }
 
-void ShoppingCart::RemoveItemFromCart(string itemName) { }
+void ShoppingCart::RemoveItemFromCart(string itemName) 
+{
+	for (int i = 0; i < items.size(); i++)
+	{
+		if (items.at(i).GetName() == itemName)
+		{
+			items.erase(items.begin() + i);
+		}
+	}
+}
 
-void ShoppingCart::UpdateItemQuantity(string, int) { }
+void ShoppingCart::UpdateItemQuantity(string itemName, int newQuantity) 
+{
+	for (int i = 0; i < items.size(); i++)
+	{
+		if (items.at(i).GetName() == itemName)
+		{
+			items.at(i).SetQuantity = newQuantity;
+		}
+	}
+}
 
-int ShoppingCart::GetNumOfItems() { }
+int ShoppingCart::GetNumOfItems() 
+{
+	int numOfItems = 0;
 
-double ShoppingCart::GetTotalCost() { }
+	for (int i = 0; i < items.size(); i++)
+	{
+		numOfItems += items.at(i).GetQuantity();
+	}
 
-void ShoppingCart::PrintItemTotalsAndCosts() { }
+	return numOfItems;
+}
 
-void ShoppingCart::PrintItemDescriptions() { }
+double ShoppingCart::GetTotalCost() 
+{ 
+	double totalCost = 0.0;
+
+	for (int i = 0; i < items.size(); i++)
+	{
+		totalCost += items.at(i).GetPrice();
+	}
+
+	return totalCost;
+}
+
+void ShoppingCart::PrintItemTotalsAndCosts() 
+{
+	cout << "Number of items: " << GetNumOfItems() << endl;
+	cout << endl;
+	
+	for (int i = 0; i < items.size(); i++)
+	{
+		items.at(i).PrintCost();
+	}
+}
+
+void ShoppingCart::PrintItemDescriptions() 
+{ 
+	cout << "Item Descriptions"<< endl;
+	cout << endl;
+
+	for (int i = 0; i < items.size(); i++)
+	{
+		items.at(i).PrintDescription();
+	}
+}
